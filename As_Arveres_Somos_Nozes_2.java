89a17a8e63b8d77467426df66b36ac0b5ce786a5
Sérgio Soares <111542611+SergioS0ares@users.noreply.github.com>
2023-09-06 20:08:49 -0300

    Add files via upload
    no primeiro dia o senhor disse:
    que se faça o projeto!
    e houve o projeto.

---
+++ As_Arveres_Somos_Nozes_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package as_arveres_somos_nozes_2;
+
+/**
+ *
+ * @author aluno
+ */
+public class As_Arveres_Somos_Nozes_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
